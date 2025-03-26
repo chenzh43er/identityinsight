bc00ecf9d3e8064314f1094dd6e7d9c87d84ac20
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-03-26 10:08:12 +0800

    update fix

---
+++ testname/js/google_adv.js
@@ -0,0 +1,19 @@
+function returnADV_Block(){
+    return `
+    <ins class="adsbygoogle"
+                             style="display:block"
+                             data-ad-client="ca-pub-3481735481590354"
+                             data-ad-slot="7945649647"
+                             data-ad-format="auto"
+                             data-full-width-responsive="true"></ins>`
+}
+
+function returnZhongxiangGuding(){
+    let advHtml = `
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:600px"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="3165596689"></ins>
+            `;
+    return advHtml;
+}
