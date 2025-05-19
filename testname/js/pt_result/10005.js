94946e97cf03023029b905215af94148382ca6a1
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-05-19 17:58:27 +0800

    1

--- testname/js/pt_result/10005.js
+++ testname/js/pt_result/10005.js
@@ -3,24 +3,24 @@ const results = {
         title:"O Que a Sua Posição de Dormir Revela Sobre a Sua Vida Amorosa? 💞🌙 ",
         resultTitle: "O Romântico Comprometido",
         description: "Você ama estar perto de quem ama, e a conexão emocional é sua prioridade em um relacionamento. Você é alguém que presta atenção a todos os detalhes, valorizando a intimidade e o tempo de qualidade. Para você, o amor está nos pequenos gestos diários.",
-        mainPic:"https://identityinsight.org/testCommon7/img/pt_img/ae7d11a9a4fa5356e73aa48a1ef60d35.jpeg"
+        mainPic:"./testname/img/pt_img/ae7d11a9a4fa5356e73aa48a1ef60d35.jpeg"
     },
     2: {
         title:"O Que a Sua Posição de Dormir Revela Sobre a Sua Vida Amorosa? 💞🌙 ",
         resultTitle: "O Prático Independente",
         description: "Você gosta de companhia, mas também valoriza seu espaço pessoal e paz em um relacionamento. Você é alguém que lida bem com tensões, mantendo o relacionamento equilibrado. O tempo compartilhado é importante, mas você precisa do seu espaço para recarregar.",
-        mainPic:"https://identityinsight.org/testCommon7/img/pt_img/1fb0e2c7ef211ba2ec93d316c58a37c9.jpeg"
+        mainPic:"./testname/img/pt_img/1fb0e2c7ef211ba2ec93d316c58a37c9.jpeg"
     },
     3: {
         title:"O Que a Sua Posição de Dormir Revela Sobre a Sua Vida Amorosa? 💞🌙 ",
         resultTitle: "O Romântico Protetor",
         description: "Você adora sentir-se próximo do seu parceiro, estando sempre presente para oferecer apoio e proteção. Você também aprecia o calor e a proximidade do amor, garantindo que seu relacionamento seja acolhedor e caloroso.",
-        mainPic:"https://identityinsight.org/testCommon7/img/pt_img/309e519ed02218d1aeb3232b0ef658e2.jpeg"
+        mainPic:"./testname/img/pt_img/309e519ed02218d1aeb3232b0ef658e2.jpeg"
     },
     4: {
         title:"O Que a Sua Posição de Dormir Revela Sobre a Sua Vida Amorosa? 💞🌙 ",
         resultTitle: "O Líder Confiante",
         description: "Você gosta de definir seu próprio espaço e tomar a iniciativa no relacionamento. Você se sente seguro em seu amor e independência, mantendo o equilíbrio perfeito entre afeição e autonomia.",
-        mainPic:"https://identityinsight.org/testCommon7/img/pt_img/f2944dffb9cd6eeb5e9007c1f5fe59f6.jpeg"
+        mainPic:"./testname/img/pt_img/f2944dffb9cd6eeb5e9007c1f5fe59f6.jpeg"
     },
 };
