5443004ebf658aa9ec83228a807b5b2f2d3f7a0c
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-05-27 16:52:24 +0800

    google adv update

--- testname/js/google_adv.js
+++ testname/js/google_adv.js
@@ -28,25 +28,120 @@ function returnZhongxiangGuding(){
             `;
     return advHtml;
 }
+//data-adtest="on" 复制该语句到为止为测试模式开启，删掉该语句则为正式广告
 
-function returnZhongxiangGuding2(){
-    let advHtml = `
+function returnListAdv_1(){
+    return `<!-- list-1 -->
+<ins class="adsbygoogle"
+     style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="7760254681"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnListAdv_2(){
+    return `<!-- list-2 -->
+<ins class="adsbygoogle"
+     style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="6447173015"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnListAdv_3(){
+    return `<!-- list-3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="9263465087"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnListAdv_4(){
+    return `<!-- list-4 -->
+<ins class="adsbygoogle"
+     style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="6538913607"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnTestAdv_1(){
+    return `<!-- test-1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="7543798111"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnTestAdv_2(){
+    return `<!-- test-2 -->
 <ins class="adsbygoogle"
      style="display:inline-block;width:100%;height:250px"
      data-ad-client="ca-pub-3481735481590354"
-     data-ad-slot="3165596689"></ins>
-            `;
-    return advHtml;
+     data-ad-slot="3912750264"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
 }
 
-function returnZhongxiangGuding3(){
-    let advHtml = `
+function returnTestAdv_3(){
+    return `<!-- test-3 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:100%;height:250px"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="5628915364"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnTestAdv_4(){
+    return `<!-- test-4 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:600px"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="2599668591"></ins>`
+}
+
+function returnResultAdv_1(){
+    return `<!-- result-1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="1286586926"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnResultAdv_2(){
+    return `<!-- result-2 -->
+<ins class="adsbygoogle"
+     style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="5134091346"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnResultAdv_3(){
+    return `<!-- result-3 -->
 <ins class="adsbygoogle"
      style="display:inline-block;width:100%;height:200px"
      data-ad-client="ca-pub-3481735481590354"
-     data-ad-slot="3165596689"></ins>
-            `;
-    return advHtml;
+     data-ad-slot="2627347502"></ins>`
 }
 
-//data-adtest="on" 复制该语句到为止为测试模式开启，删掉该语句则为正式广告
+function returnResultAdv_4(){
+    return `<!-- result-4 -->
+<ins class="adsbygoogle"
+     style="display:block;width: 100%;min-height: 250px;margin: 0 0 0 0 !important;"
+     data-ad-client="ca-pub-3481735481590354"
+     data-ad-slot="8973505259"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
