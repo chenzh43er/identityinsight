e8cc2cc5455884d0c18b0deecba3f40f0758521f
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-10-13 17:38:36 +0800

    1

--- fr/testname/js/ar_result/100001.js
+++
@@ -1,7 +0,0 @@
-const results = {
-    "1": {
-        "title": "كيف ستبدو عندما تصل إلى 60 عامًا؟",
-        "description": "مولان:",
-        "mainPic": "./testname/img/ar_img/74581858e4079322aa7c455d12c99e12.png"
-    }
-}
