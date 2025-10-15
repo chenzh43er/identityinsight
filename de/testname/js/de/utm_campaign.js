3a42fa29680fdce3300836484c9d1442c528d02c
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-10-16 03:16:55 +0800

    1

--- de/testname/js/de/utm_campaign.js
+++ de/testname/js/de/utm_campaign.js
@@ -24,11 +24,12 @@ const utm_campaignData = {
         "pic": "./testname/img/de_img/735a575acde713df117f43d8defde53b.gif",
         "title": "Welche Disney-Prinzessin bist du?"
     },
-    "hog":[{
+    "hog":[
+        {
         "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10035",
         "pic": "./testname/img/de_img/ac9db7dc2c44cba4d55016a1a313afb5.gif",
         "title": "Welches Hogwarts-Haus Passt Am Besten Zu Dir?"
-    },
+        },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10036",
             "pic": "./testname/img/de_img/b677afdd76e0057cd2df6f074beb2817.gif",
@@ -67,5 +68,46 @@ const utm_campaignData = {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10045",
             "pic": "./testname/img/de_img/dA4hK0aA4vA0.jpg",
             "title": "Welcher Hogwarts-Schüler bist du in der Liebe?"
-        }]
+        }],
+    "hog_rc":[
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10030",
+            "pic": "./testname/img/fr_img/ace397c48efa2482f39e547105d095f1.jpeg",
+            "title": "Quel est votre Patronus? Découvrez-le maintenant!"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10032",
+            "pic": "./testname/img/fr_img/334c6206304c2b879070f3e04145e218.gif",
+            "title": "À quel point connaissez-vous Harry Potter ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10033",
+            "pic": "./testname/img/fr_img/ce833ce4bb4e9334a19fcbab159d02a5.jpg",
+            "title": "À quelle maison de Poudlard appartenez-vous vraiment ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10034",
+            "pic": "./testname/img/fr_img/abea28a8a67f17b886c24b8ab4f5c0c9.gif",
+            "title": "Dans Quel Univers Devriez-Vous Vivre?"
+        },{
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10042",
+            "pic": "./testname/img/fr_img/bb705ba7746583fd7406bbcf2b8d5d6b.gif",
+            "title": "À quel professeur de Poudlard ressembles-tu le plus ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10043",
+            "pic": "./testname/img/fr_img/ce3d6f280cd66bb4810738b07f2cf8e6.gif",
+            "title": "Êtes-vous *éligible* pour un certificat officiel de Poudlard ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10044",
+            "pic": "./testname/img/fr_img/2025-10-15_204806_876.jpg",
+            "title": "Quel est ton vrai talent magique à Poudlard ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10045",
+            "pic": "./testname/img/fr_img/dA4hK0aA4vA0.jpg",
+            "title": "Quel Élève de Poudlard Es-tu en Amour ?"
+        }
+    ]
 };
