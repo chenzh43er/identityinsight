ebf21f141a73136ddfe770ddae564dc71b93d70b
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-10-26 16:00:00 +0800

    1

--- de/testname/js/de_result/10051.js
+++ de/testname/js/de_result/10051.js
@@ -1,225 +1,26 @@
-let quizData = {
-    "title": "Welcher magische Wettkämpfer wärst du in Hogwarts?",
-    "mainPic": "./testname/img/de_img/19_ce9826fd-55fc-4820-be2c-889667f3bac6.jpeg",
-    "thems": [
-        "Harry Potter",
-        "Harry-Potter-Quiz",
-        "Hogwarts",
-        "Magie",
-        "Filme",
-        "Harry-Potter-Fans",
-        "Magische Welt",
-        "Potterheads"
-    ],
-    "questions": [
-        {
-            "question": "Dein Lieblings-Hogwarts-Sport？",
-            "pic": "./testname/img/de_img/18_d28e5bf7-cd7c-4de2-9f99-412e138748f1.jpeg",
-            "answers": [
-                {
-                    "answer": "Quidditch",
-                    "aPic": "",
-                    "aId": 1
-                  },
-                  {
-                    "answer": "Zauberduell",
-                    "aPic": "",
-                    "aId": 2
-                  },
-                  {
-                    "answer": "Rätsel & Denksport",
-                    "aPic": "",
-                    "aId": 3
-                  },
-                  {
-                    "answer": "Teamaufgaben",
-                    "aPic": "",
-                    "aId": 4
-                  }
-            ]
-        },
-        {
-            "question": "Dein bevorzugtes Element?",
-            "pic": "./testname/img/de_img/17_d90ccced-7540-4e5e-9745-6244ced2c5a9.jpeg",
-            "answers": [
-                {
-                    "answer": "Feuer",
-                    "aPic": "",
-                    "aId": 1
-                  },
-                  {
-                    "answer": "Luft",
-                    "aPic": "",
-                    "aId": 2
-                  },
-                  {
-                    "answer": "Erde",
-                    "aPic": "",
-                    "aId": 3
-                  },
-                  {
-                    "answer": "Wasser",
-                    "aPic": "",
-                    "aId": 4
-                  }
-            ]
-        },
-        {
-            "question": "Welche Eigenschaft macht dich zum Sieger?",
-            "pic": "./testname/img/de_img/16_3f451fcc-f044-4aa4-90e3-0523ea17dd1b.jpeg",
-            "answers": [
-                   {
-                        "answer": "Mut",
-                        "aPic": "",
-                        "aId": 1
-                      },
-                      {
-                        "answer": "Cleverness",
-                        "aPic": "",
-                        "aId": 2
-                      },
-                      {
-                        "answer": "Teamgeist",
-                        "aPic": "",
-                        "aId": 3
-                      },
-                      {
-                        "answer": "Strategie",
-                        "aPic": "",
-                        "aId": 4
-                      }
-            ]
-        },
-        {
-            "question": "Wie gehst du an Prüfungen heran?",
-            "pic": "./testname/img/de_img/15_f0cd0993-885b-4642-b52b-c5acf7169be7.jpeg",
-            "answers": [
-                  {
-                        "answer": "Sofort handeln",
-                        "aPic": "",
-                        "aId": 1
-                      },
-                      {
-                        "answer": "Planen",
-                        "aPic": "",
-                        "aId": 2
-                      },
-                      {
-                        "answer": "Kooperation",
-                        "aPic": "",
-                        "aId": 3
-                      },
-                      {
-                        "answer": "Listig vorgehen",
-                        "aPic": "",
-                        "aId": 4
-                      }
-            ]
-        },
-        {
-            "question": "Dein bevorzugtes magisches Wesen als Partner",
-            "pic": "./testname/img/de_img/14_58c4c651-59cf-4dc3-93a8-bbef63d2eb32.jpeg",
-            "answers": [
-                 {
-                    "answer": "Hippogreif",
-                    "aPic": "",
-                    "aId": 1
-                  },
-                  {
-                    "answer": "Drache",
-                    "aPic": "",
-                    "aId": 2
-                  },
-                  {
-                    "answer": "Hund",
-                    "aPic": "",
-                    "aId": 3
-                  },
-                  {
-                    "answer": "Schlange",
-                    "aPic": "",
-                    "aId": 4
-                  }
-            ]
-        },
-        {
-            "question": "Dein stärkstes Talent?",
-            "pic": "./testname/img/de_img/13_c2013b88-7e0b-4a14-af61-c125cfd10a9b.jpeg",
-            "answers": [
-                  {
-                        "answer": "Schnelligkeit",
-                        "aPic": "",
-                        "aId": 1
-                      },
-                      {
-                        "answer": "Intelligenz",
-                        "aPic": "",
-                        "aId": 2
-                      },
-                      {
-                        "answer": "Treue",
-                        "aPic": "",
-                        "aId": 3
-                      },
-                      {
-                        "answer": "Taktik",
-                        "aPic": "",
-                        "aId": 4
-                      }
-            ]
-        },
-        {
-            "question": "Was treibt dich an?",
-            "pic": "./testname/img/de_img/12_0f33b829-4e2f-4620-8ad9-2572f607e45a.jpeg",
-            "answers": [
-                {
-                    "answer": "Ruhm",
-                    "aPic": "",
-                    "aId": 1
-                  },
-                  {
-                    "answer": "Wissen",
-                    "aPic": "",
-                    "aId": 2
-                  },
-                  {
-                    "answer": "Freundschaft",
-                    "aPic": "",
-                    "aId": 3
-                  },
-                  {
-                    "answer": "Macht",
-                    "aPic": "",
-                    "aId": 4
-                  }
-            ]
-        },
-        {
-            "question": "Dein Motto?",
-            "pic": "./testname/img/de_img/24_53221d12-fca4-485c-bad2-b596f9e91bec.jpeg",
-            "answers": [
-                  {
-                        "answer": "„Wer mutig ist, gewinnt.“",
-                        "aPic": "",
-                        "aId": 1
-                      },
-                      {
-                        "answer": "„Klug gewinnt.“",
-                        "aPic": "",
-                        "aId": 2
-                      },
-                      {
-                        "answer": "„Gemeinsam unschlagbar.“",
-                        "aPic": "",
-                        "aId": 3
-                      },
-                      {
-                        "answer": "„Strategie entscheidet.“",
-                        "aPic": "",
-                        "aId": 4
-                      }
-            ]
-        }
-    ],
-    "toAnswer": 10051
+const results = {
+    1: {
+        "title": "Welcher magische Wettkämpfer wärst du in Hogwarts?",
+        "resultTitle": "Der Kämpfer",
+        "description": `Furchtlos und energiegeladen, du liebst Action und Prüfungen.`,
+        "mainPic": "./testname/img/de_img/10_070f9b5d-dff2-4c21-8ca6-a599cf483b27.jpeg"
+    },
+    2: {
+        "title": "Welcher magische Wettkämpfer wärst du in Hogwarts?",
+        "resultTitle": "Der Denker",
+        "description": `Clever, analytisch, du löst jede Herausforderung mit Köpfchen.`,
+        "mainPic": "./testname/img/de_img/09_c628dc52-c4ba-477f-83fc-44e0ce280f39.jpeg"
+    },
+    3: {
+        "title": "Welcher magische Wettkämpfer wärst du in Hogwarts?",
+        "resultTitle": "Der Teamplayer",
+        "description": `Loyal, hilfsbereit, du glänzt im Team und beschützt andere.`,
+        "mainPic": "./testname/img/de_img/08_3e4a1c7b-34a3-40e3-bccd-2a64296c31ef.jpeg"
+    },
+    4: {
+        "title": "Welcher magische Wettkämpfer wärst du in Hogwarts?",
+        "resultTitle": "Der Stratege",
+        "description": `Taktisch und clever, du planst jeden Schritt bis zum Sieg.`,
+        "mainPic": "./testname/img/de_img/07_003c601f-e080-4d34-890c-f2cbdcd56509.jpeg"
+    },
 };
