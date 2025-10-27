6df5a277ade286c2006f26d7f6fbf6c56e3d1192
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-10-27 20:13:09 +0800

    1

--- fr/testname/js/fr/utm_campaign.js
+++ fr/testname/js/fr/utm_campaign.js
@@ -119,6 +119,101 @@ const utm_campaignData = {
             "title": "Quel personnage principal de Harry Potter êtes-vous ?"
         }
         ],
+    "hog_main":[
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10052",
+            "pic": "./testname/img/fr_img/06_921e1914-5947-435e-98cc-63c5a3410d36.jpeg",
+            "title": "✨ Quelle face cachée de ta magie Poudlard révélerait-elle ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10053",
+            "pic": "./testname/img/fr_img/19_53253b01-c20f-4349-8864-85eac1f6d65d.jpeg",
+            "title": "Que te montrerait le Miroir du Riséd ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10050",
+            "pic": "./testname/img/fr_img/06_08d508af-86c4-4d6e-91c9-8035c0b4df56.jpeg",
+            "title": "Quel animal magique de Harry Potter serait ton compagnon ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10051",
+            "pic": "./testname/img/fr_img/19_ce9826fd-55fc-4820-be2c-889667f3bac6.jpeg",
+            "title": "⚡ Quel compétiteur magique serais-tu à Poudlard ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10049",
+            "pic": "./testname/img/fr_img/06_70dce5ae-2d78-42c6-93a5-c40fb437400e.jpeg",
+            "title": "Maîtriserais-tu les Forces du Mal ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10047",
+            "pic": "./testname/img/fr_img/06_85cc12b1-1b9c-40d4-8f16-474c55167de5.png",
+            "title": "Quel méchant de Harry Potter sommeille en toi ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10048",
+            "pic": "./testname/img/fr_img/19_e52f591e-3d5e-4443-b754-60c6cb8d70f0.jpeg",
+            "title": "Quel artefact magique te choisirait ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10031",
+            "pic": "./testname/img/fr_img/ac9db7dc2c44cba4d55016a1a313afb5.gif",
+            "title": "Quelle Maison de Poudlard Vous Convient le Mieux?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10028",
+            "pic": "./testname/img/fr_img/b677afdd76e0057cd2df6f074beb2817.gif",
+            "title": "Êtes-vous plus une fille de Poudlard ou une fille d'Oz ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10029",
+            "pic": "./testname/img/fr_img/6b8d7b30c12ec88bf4a563c898243625.jpg",
+            "title": "Testez Vos Connaissances en Anime et Découvrez Quel Personnage de Harry Potter Vous Êtes Vraiment"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10030",
+            "pic": "./testname/img/fr_img/ace397c48efa2482f39e547105d095f1.jpeg",
+            "title": "Quel est votre Patronus? Découvrez-le maintenant!"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10032",
+            "pic": "./testname/img/fr_img/334c6206304c2b879070f3e04145e218.gif",
+            "title": "À quel point connaissez-vous Harry Potter ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10033",
+            "pic": "./testname/img/fr_img/ce833ce4bb4e9334a19fcbab159d02a5.jpg",
+            "title": "À quelle maison de Poudlard appartenez-vous vraiment ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10034",
+            "pic": "./testname/img/fr_img/abea28a8a67f17b886c24b8ab4f5c0c9.gif",
+            "title": "Dans Quel Univers Devriez-Vous Vivre?"
+        },{
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10042",
+            "pic": "./testname/img/fr_img/bb705ba7746583fd7406bbcf2b8d5d6b.gif",
+            "title": "À quel professeur de Poudlard ressembles-tu le plus ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10043",
+            "pic": "./testname/img/fr_img/ce3d6f280cd66bb4810738b07f2cf8e6.gif",
+            "title": "Êtes-vous *éligible* pour un certificat officiel de Poudlard ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10044",
+            "pic": "./testname/img/fr_img/2025-10-15_204806_876.jpg",
+            "title": "Quel est ton vrai talent magique à Poudlard ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10045",
+            "pic": "./testname/img/fr_img/dA4hK0aA4vA0.jpg",
+            "title": "Quel Élève de Poudlard Es-tu en Amour ?"
+        },{
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10046",
+            "pic": "./testname/img/fr_img/935b51532edd5d9ab67b4c97b6e6e9a9.gif",
+            "title": "Quel personnage principal de Harry Potter êtes-vous ?"
+        }
+    ],
     "hog_rc":[
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10052",
@@ -199,5 +294,86 @@ const utm_campaignData = {
                 "pic": "./testname/img/fr_img/935b51532edd5d9ab67b4c97b6e6e9a9.gif",
                 "title": "Quel personnage principal de Harry Potter êtes-vous ?"
             }
-        ]
+        ],
+    "hog_mt":[
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10052",
+            "pic": "./testname/img/fr_img/06_921e1914-5947-435e-98cc-63c5a3410d36.jpeg",
+            "title": "✨ Quelle face cachée de ta magie Poudlard révélerait-elle ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10053",
+            "pic": "./testname/img/fr_img/19_53253b01-c20f-4349-8864-85eac1f6d65d.jpeg",
+            "title": "Que te montrerait le Miroir du Riséd ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10050",
+            "pic": "./testname/img/fr_img/06_08d508af-86c4-4d6e-91c9-8035c0b4df56.jpeg",
+            "title": "Quel animal magique de Harry Potter serait ton compagnon ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10051",
+            "pic": "./testname/img/fr_img/19_ce9826fd-55fc-4820-be2c-889667f3bac6.jpeg",
+            "title": "⚡ Quel compétiteur magique serais-tu à Poudlard ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10049",
+            "pic": "./testname/img/fr_img/06_70dce5ae-2d78-42c6-93a5-c40fb437400e.jpeg",
+            "title": "Maîtriserais-tu les Forces du Mal ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10047",
+            "pic": "./testname/img/fr_img/06_85cc12b1-1b9c-40d4-8f16-474c55167de5.png",
+            "title": "Quel méchant de Harry Potter sommeille en toi ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10048",
+            "pic": "./testname/img/fr_img/19_e52f591e-3d5e-4443-b754-60c6cb8d70f0.jpeg",
+            "title": "Quel artefact magique te choisirait ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10030",
+            "pic": "./testname/img/fr_img/ace397c48efa2482f39e547105d095f1.jpeg",
+            "title": "Quel est votre Patronus? Découvrez-le maintenant!"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10032",
+            "pic": "./testname/img/fr_img/334c6206304c2b879070f3e04145e218.gif",
+            "title": "À quel point connaissez-vous Harry Potter ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10033",
+            "pic": "./testname/img/fr_img/ce833ce4bb4e9334a19fcbab159d02a5.jpg",
+            "title": "À quelle maison de Poudlard appartenez-vous vraiment ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10034",
+            "pic": "./testname/img/fr_img/abea28a8a67f17b886c24b8ab4f5c0c9.gif",
+            "title": "Dans Quel Univers Devriez-Vous Vivre?"
+        },{
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10042",
+            "pic": "./testname/img/fr_img/bb705ba7746583fd7406bbcf2b8d5d6b.gif",
+            "title": "À quel professeur de Poudlard ressembles-tu le plus ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10043",
+            "pic": "./testname/img/fr_img/ce3d6f280cd66bb4810738b07f2cf8e6.gif",
+            "title": "Êtes-vous *éligible* pour un certificat officiel de Poudlard ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10044",
+            "pic": "./testname/img/fr_img/2025-10-15_204806_876.jpg",
+            "title": "Quel est ton vrai talent magique à Poudlard ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10045",
+            "pic": "./testname/img/fr_img/dA4hK0aA4vA0.jpg",
+            "title": "Quel Élève de Poudlard Es-tu en Amour ?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10046",
+            "pic": "./testname/img/fr_img/935b51532edd5d9ab67b4c97b6e6e9a9.gif",
+            "title": "Quel personnage principal de Harry Potter êtes-vous ?"
+        }
+    ]
 };
