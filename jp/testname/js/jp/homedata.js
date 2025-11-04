21515dfc5e4b53795ed2b52bb0c96de57f54ac79
chenzh43er <751594842@qq.com>
2025-11-04 21:27:44 +0800

    1

--- jp/testname/js/jp/homedata.js
+++ jp/testname/js/jp/homedata.js
@@ -163,5 +163,172 @@ const dataPage = {
 			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10033",
 			"pic": "./testname/img/jp_img/b6124b5d49ca37d5996bf8501f26da40.gif",
 			"title": "あなたはどのカプチンカオスのアーキタイプですか？"
-		}]
+		}],
+	"allRecent":[
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10033",
+			"pic": "./testname/img/jp_img/b6124b5d49ca37d5996bf8501f26da40.gif",
+			"title": "あなたはどのカプチンカオスのアーキタイプですか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10032",
+			"pic": "./testname/img/jp_img/912d8222fd7e3d6b18f5d85d58de59ca.gif",
+			"title": "他の人は私をどう見ている?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10031",
+			"pic": "./testname/img/jp_img/177dccb42f0a27ab25194cae3a018a02.gif",
+			"title": "ロブロックスであなたはどのファニーインサイドアウトロールプレイにいますか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10030",
+			"pic": "./testname/img/jp_img/907c279d8858b0d6053bfb83543570a1.jpeg",
+			"title": "インサイド・ヘッド2でのあなたの感情は？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10029",
+			"pic": "./testname/img/jp_img/6a03fa96e13b79bd2663905ea35412cd.gif",
+			"title": "Robloxでどんな庭を作りますか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10028",
+			"pic": "./testname/img/jp_img/a246afc0181b8e909eab5b53771ebcbf.gif",
+			"title": "フォートナイトでどのイカゲームのスキンですか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10027",
+			"pic": "./testname/img/jp_img/d2b2ac3c45857ef4a3c68b34bcca2958.gif",
+			"title": "実際にはどのくらいの年齢に見えますか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10026",
+			"pic": "./testname/img/jp_img/ff268009224aeaeb6782fd235bde71c3.gif",
+			"title": "ソニック3のどの戦士があなたに最も適していますか？ 🚀💎"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10025",
+			"pic": "./testname/img/jp_img/c209c4c028da74398ce1ea05a1ffca55.gif",
+			"title": "あなたの美学にマッチするソニック・ザ・ヘッジホッグ 3 のグローアップはどれ？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10024",
+			"pic": "./testname/img/jp_img/7a0ea7fac3add6f4576223912c481587.gif",
+			"title": "あなたはどの斉木楠雄のΨ難のキャラクター？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10023",
+			"pic": "./testname/img/jp_img/28bbd8689d5b3d9f2fa4ef84a66801ea.gif",
+			"title": "君はどの『ソロ生活』のキャラクター？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10022",
+			"pic": "./testname/img/jp_img/29f8706244bc5e59eac1927809dcbb35.gif",
+			"title": "FNF リライト ラウンド2であなたはどの暗黒キャラクターですか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10021",
+			"pic": "./testname/img/jp_img/00b91c6afb01e2d569032621e1736de7.gif",
+			"title": "呪術廻戦の隠れた詳細について知っていますか？自分を挑戦してみましょう。"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10020",
+			"pic": "./testname/img/jp_img/acc6be7569e6cda33ab71b16039afe37.gif",
+			"title": "ドンキーコングバナンザ: あなたはどんなプレイヤー？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10019",
+			"pic": "./testname/img/jp_img/fb152127163e27803da62c65edf79fdc.gif",
+			"title": "どのGTA MODライフがあなたに適していますか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10018",
+			"pic": "./testname/img/jp_img/d1589d2822bb5f96f107d2d3ec5036a0.gif",
+			"title": "イカゲーム3を生き残れるか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10017",
+			"pic": "./testname/img/jp_img/f5a759fac9d5c9eb8f55e8cfadd15d3f.gif",
+			"title": "ファミリーガイのどのキャラクターがあなたの性格に合いますか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10016",
+			"pic": "./testname/img/jp_img/7e577b2fce96000b0e2b6c714ff65207.gif",
+			"title": "あなたはどのマーベルライバルヒーローですか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10015",
+			"pic": "./testname/img/jp_img/7c2813f835d9ce4fa734f5ac4866b356.gif",
+			"title": "どのヴァンパイアハンターDのキャラクターがあなたに最も似ている？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10014",
+			"pic": "./testname/img/jp_img/f1ee60c7c37a3fbaac6b9ea6cb17380f.gif",
+			"title": "あなたの内なる子供を映し出すアニメキャラクターは？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10013",
+			"pic": "./testname/img/jp_img/7242dd62fbbbf28d368c853fd94e00e0.jpeg",
+			"title": "あなたはどのジブリキャラクターですか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10012",
+			"pic": "./testname/img/jp_img/e4a5a7e98b0066ef4666e80b161d5e67.jpg",
+			"title": "ディズニーズーを作り、あなたの内面的なディズニーサイドキックのパーソナリティを見つけてください<ud83d>デ4a"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10011",
+			"pic": "./testname/img/jp_img/332d9bfe7401813405d03eedd24bf253.jpeg",
+			"title": "あなたはどのワンパンマンのキャラクターですか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10010",
+			"pic": "./testname/img/jp_img/9354da22ff2aeca7102e93aa4e9fd48c.gif",
+			"title": "どのDAN DA DANがあなたの完璧なソウルメイト？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10009",
+			"pic": "./testname/img/jp_img/f8577e95c27a7403c2bdfbb099124426.gif",
+			"title": "鬼滅の刃:無限城であなたはどのキャラクター？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10008",
+			"pic": "./testname/img/jp_img/fce9653564bb39910495a489b892aac6.gif",
+			"title": "あなたはどの巨人ですか？進撃の巨人からあなたの巨人シフターフォームを見つけてください"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10007",
+			"pic": "./testname/img/jp_img/218387c6f9aea09748aca95046de7e72.gif",
+			"title": "鬼滅の刃：無限城から脱出できますか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10006",
+			"pic": "./testname/img/jp_img/008302f3f23793666eb7adb64ec4b845.gif",
+			"title": "鬼滅の刃キャラクターの「キル、マリー、キス」は？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10005",
+			"pic": "./testname/img/jp_img/d2c033f7aefc629af9c027adb29f4535.gif",
+			"title": "I Am Cat VR：このクイズであなたの本当の猫の性格を見つけましょう。"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10004",
+			"pic": "./testname/img/jp_img/02a34a87a28eda0a9c75823456eb6e3c.gif",
+			"title": "どのRobloxアバターがあなたの雰囲気に合う？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10003",
+			"pic": "./testname/img/jp_img/7245e30d6476788d05fa94fbf7e0b7ab.gif",
+			"title": "私たちは「My Hero Academia S7」のあなたの双子を見つけました！ 誰であるかを見る準備はできていますか？"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10002",
+			"pic": "./testname/img/jp_img/20aad335769c9b8ac5a473c7247ca706.gif",
+			"title": "ワンピースクイズ - あなたの賞金首は何ですか？ 💸"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10001",
+			"pic": "./testname/img/jp_img/4898a8fd657b8a0db2335f9944d053c9.gif",
+			"title": "あなたの呪術廻戦3の彼氏は誰？"
+		}
+	]
 }
--- pt/testname/js/pt/homedata.js
+++ pt/testname/js/pt/homedata.js
@@ -133,5 +133,137 @@ const dataPage = {
 			"url": "https://identityinsight.org/pt/test-page_pt/?utm_content=10042",
 			"pic": "https://identityinsight.org/testCommon4/img/pt_img/2ae7524f7b028c8635a4f71decffc8cd.gif",
 			"title": "Qual Deus Mítico Te Abençoou?"
-		}]
+		}],
+	"allRecent":[
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10047",
+			"pic": "./testname/img/pt_img/cc6709eadd1cb8705db69a773c8f6f5e.gif",
+			"title": "Quais Volumes de Música Sprunki Incredibox Refletem Seu Lado Interior?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10046",
+			"pic": "./testname/img/pt_img/02a34a87a28eda0a9c75823456eb6e3c.gif",
+			"title": "Qual Avatar do Roblox Combina com Seu Estilo?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10045",
+			"pic": "./testname/img/pt_img/9512f4c9566e937dc8c4a40eb39b3925.gif",
+			"title": "Qual é o seu nível de futebol no VRFS?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10042",
+			"pic": "./testname/img/pt_img/2ae7524f7b028c8635a4f71decffc8cd.gif",
+			"title": "Qual Deus Mítico Te Abençoou?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10041",
+			"pic": "./testname/img/pt_img/909887cfd5d9e1ef3648ea7051ada2c0.gif",
+			"title": "Quem é seu crush masculino de personagem da Marvel?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10040",
+			"pic": "./testname/img/pt_img/951b17d04b8d781a2c33eb9f6d3f3c30.gif",
+			"title": "Qual Herói da Marvel Reflete Suas Boas e Más Características?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10039",
+			"pic": "./testname/img/pt_img/23c5ae8244c5f838ac6df53605f25c35.gif",
+			"title": "Qual animatrônico de Five Nights At Freddy's você seria no Toca Life?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10038",
+			"pic": "./testname/img/pt_img/b576911a28e8afca90e64c61da568ffa.gif",
+			"title": "Seu aniversário determina sua personalidade oculta!"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10037",
+			"pic": "./testname/img/pt_img/ae65488ddb169cb68cf3b5ea59205b06.jpeg",
+			"title": "Câmera de Sósia: Com qual Celebridade Você se Parece?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10036",
+			"pic": "./testname/img/pt_img/9a0b4668d81f6a3c12e9be0622d67646.jpeg",
+			"title": "Quais São Minhas Surpresas de Abril?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10035",
+			"pic": "./testname/img/pt_img/b7ab8b14dcc4bf523dbbb70505c2f9de.jpeg",
+			"title": "Diagnóstico de Pulso da Medicina Chinesa: Teste Quantos Parceiros Você Terá"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10034",
+			"pic": "./testname/img/pt_img/3587ec0926c5d2c63bf37e582e73a32d.gif",
+			"title": "Qual é a primeira impressão do seu amante sobre você versus a impressão atual?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10033",
+			"pic": "./testname/img/pt_img/759f115373891120964d14945775f483.gif",
+			"title": "Qual personagem de filme das férias de primavera é sua alma gêmea?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10032",
+			"pic": "./testname/img/pt_img/87192201602c6a8362354e0e3c499c10.gif",
+			"title": "Preveja Sua Próxima Vida Através da Astrologia!"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10031",
+			"pic": "./testname/img/pt_img/ab4ac4fa80513401b1856f0757925ba4.gif",
+			"title": "Descubra a Mensagem de Deus Através da IA: O que Ela Diz a Você?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10030",
+			"pic": "./testname/img/pt_img/f3265065e4560dbb589996c7b249f3d9.gif",
+			"title": "Qual Animal Reflete a Personalidade da Sua Impressão Digital?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10029",
+			"pic": "./testname/img/pt_img/07e41d20159406ff2364a92b9125b1e9.jpeg",
+			"title": "Com quais símbolos de proteção você mais se identifica?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10028",
+			"pic": "./testname/img/pt_img/218fda43b8516700d8e65f4d4424f708.gif",
+			"title": "Quem é seu Alma Gêmea? Descubra a Inicial do Nome, Signo do Zodíaco, Cor do Cabelo e dos Olhos!"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10027",
+			"pic": "./testname/img/pt_img/a62cdf66859f02543bc5c2f8bdd4e741.gif",
+			"title": "Qual Legume Você É Baseado na Sua Impressão Digital?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10026",
+			"pic": "./testname/img/pt_img/af609e6c39e7382a63fe7001b30d5f5f.gif",
+			"title": "O Que a Sua Impressão Digital Revela Sobre o Nome do Seu Alma Gêmea?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10025",
+			"pic": "./testname/img/pt_img/68539001e80fd90661f6649e204a9469.gif",
+			"title": "Qual Personagem de NANA Você É?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10024",
+			"pic": "./testname/img/pt_img/e0d1db9d3708f648d36306b668df9d86.gif",
+			"title": "Qual música do Beat Saber mostra sua vibração?"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10023",
+			"pic": "./testname/img/pt_img/dd1da2d2e2fa64494019288642c7d472.gif",
+			"title": "Descubra a Causa da Sua Morte em Sua Vida Passada Através de Impressões Digitais"
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10007",
+			"pic": "./testname/img/pt_img/594af3246ee1a672d5973d3744b6af0f.gif",
+			"title": "Qual Herói Combina com Suas Habilidades em Marvel Rivals? "
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10006",
+			"pic": "./testname/img/pt_img/735a575acde713df117f43d8defde53b.gif",
+			"title": "Qual princesa da Disney é você? "
+		},
+		{
+			"url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10004",
+			"pic": "./testname/img/pt_img/904b585caba8ef47d1ea92b039bb8575.gif",
+			"title": "Quantas pessoas estão apaixonadas por você na escola?"
+		}
+	]
 }
