5206e7648493ec6bcbb4742d6b60f3bc0307402a
chenzh43er <751594842@qq.com>
2025-12-03 16:54:06 +0800

    1

--- sv/testname/js/sv_most_test_data/Most Tested.js
+++ sv/testname/js/sv_most_test_data/Most Tested.js
@@ -35,6 +35,50 @@ const dataM = {
         },
     ],
     "horo":[
-
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=100120",
+            "pic": "./testname/img/sv_img/13bc72828b062229dd2750e3bafb47c4.gif",
+            "title": "Fatta beslut utifrån ditt stjärntecken och ta reda på vad din själsfrände heter!"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=100121",
+            "pic": "./testname/img/sv_img/39b596f2c024869bb393afc04c18e57a.gif",
+            "title": "Vad säger ditt veckohoroskop om ditt kärleksliv?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=100122",
+            "pic": "./testname/img/sv_img/972a4b4894d92bfb2801519b9fedc93d.gif",
+            "title": "Vilken karriär passar dig enligt ditt stjärntecken?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=100123",
+            "pic": "./testname/img/sv_img/53df6ab80eebbfec50562157fb1aab58.gif",
+            "title": "Fatta beslut baserat på ditt stjärntecken och få en ödesförutsägelse!"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=100100",
+            "pic": "./testname/img/sv_img/0f213fa34fb9cbf22db7bdca3cac56b5.gif",
+            "title": "Måntest|Är Du Kompatibel Med Din Partner?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=100101",
+            "pic": "./testname/img/sv_img/93f935ad434696438bcf8f31987b9c6b.gif",
+            "title": "Vad avslöjar dina ögon om ditt tidigare liv?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=100102",
+            "pic": "./testname/img/sv_img/5733f4c5286c61332b3a744289e4ab79.gif",
+            "title": "Vem är din perfekta matchning i zodiaken på skolan? 💫🎒"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=100103",
+            "pic": "./testname/img/sv_img/33df5d5851de316b949deee8ad45bdde.gif",
+            "title": "Vilken typ av livsmanus är gömt på din födelsedag?"
+        },
+        {
+            "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=100104",
+            "pic": "./testname/img/sv_img/951707f8724e35d3feb275c5bd719b90.gif",
+            "title": "Upptäck din framtid för 2025 genom tarotkort!"
+        }
     ]
 }
