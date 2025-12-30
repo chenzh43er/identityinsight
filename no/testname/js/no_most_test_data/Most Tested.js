3575e48f62d274b2a95c19a446cb8475924c2389
chenzh43er <751594842@qq.com>
2025-12-30 22:58:31 +0800

    1

--- no/testname/js/no_most_test_data/Most Tested.js
+++ no/testname/js/no_most_test_data/Most Tested.js
@@ -33,87 +33,87 @@ const dataM = {
     "horo":[
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10061",
-            "pic": "./img/no_img/da2b8b02ba8e7cf49aea92283a7eaa24.gif",
+            "pic": "./testname/img/no_img/da2b8b02ba8e7cf49aea92283a7eaa24.gif",
             "title": "Hva vil skje med deg i 2025?"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10062",
-            "pic": "./img/no_img/ba172e27c7b6813f18385194af06fcd4.gif",
+            "pic": "./testname/img/no_img/ba172e27c7b6813f18385194af06fcd4.gif",
             "title": "Når vil du gifte deg ifølge astrologi?"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10063",
-            "pic": "./img/no_img/5733f4c5286c61332b3a744289e4ab79.gif",
+            "pic": "./testname/img/no_img/5733f4c5286c61332b3a744289e4ab79.gif",
             "title": "Hvem Er Ditt Perfekte Zodiac Match På Skolen? 💫🎒"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10064",
-            "pic": "./img/no_img/0f213fa34fb9cbf22db7bdca3cac56b5.gif",
+            "pic": "./testname/img/no_img/0f213fa34fb9cbf22db7bdca3cac56b5.gif",
             "title": "Månetest|Er du kompatibel med partneren din?"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10065",
-            "pic": "./img/no_img/fac405620d97740b238099e3c19cd24f.gif",
+            "pic": "./testname/img/no_img/fac405620d97740b238099e3c19cd24f.gif",
             "title": "Hvilken Reinkarnasjon Er Du I Nå?"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10066",
-            "pic": "./img/no_img/93f935ad434696438bcf8f31987b9c6b.gif",
+            "pic": "./testname/img/no_img/93f935ad434696438bcf8f31987b9c6b.gif",
             "title": "Hva avslører øynene dine om ditt tidligere liv?"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10067",
-            "pic": "./img/no_img/d202cce92fbe7877cc1212f67584a0d2.gif",
+            "pic": "./testname/img/no_img/d202cce92fbe7877cc1212f67584a0d2.gif",
             "title": "Drømmetyding | Vil sjelen din gå til himmelen eller helvete?"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10068",
-            "pic": "./img/no_img/0a7c50e316332bbd262fa5ec89d8b258.gif",
+            "pic": "./testname/img/no_img/0a7c50e316332bbd262fa5ec89d8b258.gif",
             "title": "Fødselsmerket ditt viser hvor du døde i et tidligere liv!"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10069",
-            "pic": "./img/no_img/68ccf7ef8a5158b47ceaf5b512cb9989.gif",
+            "pic": "./testname/img/no_img/68ccf7ef8a5158b47ceaf5b512cb9989.gif",
             "title": "Hva er sjelens element? Oppdag det nå!"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10070",
-            "pic": "./img/no_img/68ccf7ef8a5158b47ceaf5b512cb9989.gif",
+            "pic": "./testname/img/no_img/68ccf7ef8a5158b47ceaf5b512cb9989.gif",
             "title": "Hva er sjelens element? Oppdag det nå!"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10071",
-            "pic": "./img/no_img/d3dce2b2e576761a9a2a52a41391d0e3.gif",
+            "pic": "./testname/img/no_img/d3dce2b2e576761a9a2a52a41391d0e3.gif",
             "title": "Hva avslører nivået på fødselshoroskopet om deg?"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10072",
-            "pic": "./img/no_img/03791f91f1db6ee7cdf86eab64191b9a.gif",
+            "pic": "./testname/img/no_img/03791f91f1db6ee7cdf86eab64191b9a.gif",
             "title": "Beregn din kompatibilitet med partneren din basert på fødselshoroskopet ditt!"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10073",
-            "pic": "./img/no_img/e03a3428cd3a4556940c594500755100.gif",
+            "pic": "./testname/img/no_img/e03a3428cd3a4556940c594500755100.gif",
             "title": "Test din nasjonalitet gjennom håndlesing"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10074",
-            "pic": "./img/no_img/0d1fc679b60296307a78f8e0b4aba4c0.gif",
+            "pic": "./testname/img/no_img/0d1fc679b60296307a78f8e0b4aba4c0.gif",
             "title": "Hvilken stil av antrekk passer best til ditt stjernetegn?"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10075",
-            "pic": "./img/no_img/03bc5b4f002977705d02ea147f92fa1b.gif",
+            "pic": "./testname/img/no_img/03bc5b4f002977705d02ea147f92fa1b.gif",
             "title": "Hva Sier Din Zodiac Fødestein Om Deg?"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10076",
-            "pic": "./img/no_img/21b7edff05b8925ce62d27b1335fa608.gif",
+            "pic": "./testname/img/no_img/21b7edff05b8925ce62d27b1335fa608.gif",
             "title": "Hvor heldig vil ditt stjernetegn være i 2025?"
         },
         {
             "url": "https://identityinsight.org/fr/test-page_fr/?utm_content=10077",
-            "pic": "./img/no_img/1dd9ac2619cb0b137ba5184393a444af.gif",
+            "pic": "./testname/img/no_img/1dd9ac2619cb0b137ba5184393a444af.gif",
             "title": "Ta valg basert på stjernetegnet ditt og få din personlighetsanalyse!"
         }
     ]
